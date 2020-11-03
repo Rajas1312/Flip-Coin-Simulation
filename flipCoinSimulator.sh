a766548b940acc09469ca831c1428547de52a8ab
Rajas1312 <itsmerajas2@gmail.com>
2020-11-03 23:10:15 +0530

    flipping a coin 10 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,21 @@
 #!/bin/bash -x
 
 echo " Flip Coin Simulation problem"
-
-
+a=0
+b=0
+for i in {1..10}
+do
 	if [ $((RANDOM%2)) -eq 0 ]
 	then
 		echo "Heads is the winner"
+		((a++))
 
 	else
 		echo "Tails is the winner"
+		((b++))
 
 	fi
+done
+echo " $a times heads won"
+echo " $b times tails won"
 
